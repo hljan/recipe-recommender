382c2f4054f821f91c1819357aff8a36533aa3b2
vikwato <vikwatokamanga@rocketmail.com>
2021-04-08 20:13:28 +0200

    Implemented Initial Text Search HTML Template

--- RecipeApp/auth.py
+++ RecipeApp/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required
 
 auth = Blueprint('auth', __name__)
@@ -11,10 +11,10 @@ def login():
 
 @auth.route('/login', methods=['POST'])
 def login_post():
-    return 'Login'
+    return 'Logged in'
 
 
 @auth.route('/logout')
 # @login_required
 def logout():
-    return
+    return 'Logged out'
--- RecipeApp/text_search.py
+++ RecipeApp/text_search.py
@@ -4,7 +4,7 @@ from flask_login import login_required
 textSearch = Blueprint('textSearch', __name__)
 
 
-@textSearch.route('/')
+@textSearch.route('/text-search')
 # @login_required
 def text_search():
     return render_template('text_search.html')
