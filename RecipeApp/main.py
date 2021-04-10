286a98059029bbb0a5035f05ead4b00cca29a250
Jan <hl.jan109@gmail.com>
2021-04-10 13:16:44 +0200

    added DB connection

--- RecipeApp/main.py
+++ RecipeApp/main.py
@@ -1,22 +1,42 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from models import get_csv_dict
+from models import get_csv_dict, init_neo4j, test_conn
 
 # from flask_login import login_required
 
 main = Blueprint('main', __name__)
+
+ingredients_dict = dict()
+main_ingredients = list()
+side_ingredients = list()
+
+driver_neo4j = ''
 
 
 @main.route('/main/<user_id>', methods=['GET', 'POST'])
 # @login_required
 def initial_search(user_id):
     if request.method == 'GET':
-        ingredient_dict = get_csv_dict('data/ingredient_list.csv')
-        return render_template('main.html', user_id=user_id, ingredients=ingredient_dict)
+        global ingredients_dict, driver_neo4j, session_neo4j
+
+        if not ingredients_dict:
+            ingredients_dict = get_csv_dict('data/ingredient_list.csv')
+
+        if not driver_neo4j:
+            driver_neo4j = init_neo4j()
+            # only check for the first time
+            is_db_up = test_conn(driver_neo4j)
+            return render_template('main.html', user_id=user_id, ingredients=ingredients_dict, db_connected=is_db_up)
+
+        return render_template('main.html', user_id=user_id, ingredients=ingredients_dict, db_connected=True)
+
     elif request.method == 'POST':
+        global main_ingredients, side_ingredients
+
         main_ingredients = request.form['main_ingredients']
         side_ingredients = request.form['side_ingredients']
-        print(main_ingredients)
-        print(side_ingredients)
+
+        main_ingredients = list(main_ingredients[:-1].split(","))
+        side_ingredients = list(side_ingredients[:-1].split(","))
 
         if request.form['search_type'] == 'visual_search':
             return redirect(url_for('visualSearch.matching_recipes', user_id=user_id))
--- RecipeApp/models.py
+++ RecipeApp/models.py
@@ -1,11 +1,27 @@
 import pandas as pd
+from py2neo import Graph
+from neo4j import GraphDatabase
 
 
 def get_csv_dict(path='data/ingredient_list.csv'):
     ingredients = pd.read_csv(path, header=0).to_dict()
-
-    # for i in range(2):
-    #     print(ingredients['id'][i])
-    #     print(ingredients['name'][i])
-
     return ingredients
+
+
+def init_neo4j(uri='bolt://localhost:7687', auth=('neo4j', 'recipe')):
+    driver = Graph(bolt=True, host='localhost', user=auth[0], password=auth[-1])
+    # driver = GraphDatabase.driver(uri=uri, auth=auth)
+    return driver
+
+
+def test_conn(driver):
+    query = """
+            MATCH (n) 
+            RETURN n LIMIT 5
+            """
+    results = driver.run(query).to_data_frame()
+
+    if results.size == 5:
+        return True
+    else:
+        return False
