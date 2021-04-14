ac393123d714af0d90a378dd0e30833847568f04
vikwato <vikwatokamanga@rocketmail.com>
2021-04-14 23:12:06 +0200

    Added Text-Search Functionality

--- RecipeApp/text_search.py
+++ RecipeApp/text_search.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from models import get_recipes
 
 textSearch = Blueprint('textSearch', __name__)
 
@@ -7,4 +8,6 @@ textSearch = Blueprint('textSearch', __name__)
 @textSearch.route('/text-search/<user_id>')
 # @login_required
 def text_search(user_id):
-    return render_template('text_search.html')
+    recipes = get_recipes()
+    recipe_names = recipes['name']
+    return render_template('text_search.html',user_id=user_id, recipe_names=recipe_names)
