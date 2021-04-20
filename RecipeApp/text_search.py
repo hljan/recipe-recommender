5fb39c52639d3d47f6847bd7b5711d58846c3d1e
vikwato <vikwatokamanga@rocketmail.com>
2021-04-20 21:00:17 +0200

    Added Text Search to Recipe Details

--- RecipeApp/text_search.py
+++ RecipeApp/text_search.py
@@ -6,32 +6,34 @@ from flask_login import login_required
 from models import PyNeoGraph
 
 textSearch = Blueprint('textSearch', __name__)
-main_ingredients = ''
-side_ingredients = ''
-selected_recipe = ''
-driver_neo4j = ''
+main_ingredients = list()
+side_ingredients = list()
+selected_recipe = list()
+driver_neo4j = list()
 
 @textSearch.route('/text_search/<user_id>', methods=['GET', 'POST'])
 # @login_required
 def text_search(user_id):
     global main_ingredients, side_ingredients, driver_neo4j
+    main_ingredients = list()
+    side_ingredients = list()
 
     if not main_ingredients or not side_ingredients:
         main_ingredients = request.args.get('main_ingredients')
         side_ingredients = request.args.get('side_ingredients')
 
-        main_ingredients = list(main_ingredients.split(","))
-        side_ingredients = list(side_ingredients.split(","))
+        main_ingredients = list(set((main_ingredients.split(","))))
+        side_ingredients = list(set(side_ingredients.split(",")))
 
     if not driver_neo4j:
         driver_neo4j = PyNeoGraph(debug=True)
 
-    results = driver_neo4j.get_matching_recipes(main_ingredients, side_ingredients)
+    result = driver_neo4j.get_matching_recipes(main_ingredients, side_ingredients)
 
     matching_recipes =[]
 
-    for data in results:
-        matching_recipes = json.loads(results[data])
+    for data in result:
+        matching_recipes = json.loads(result[data])
 
     return render_template('text_search.html',user_id=user_id, matching_recipes=matching_recipes,
                            main_ingredients=main_ingredients,
